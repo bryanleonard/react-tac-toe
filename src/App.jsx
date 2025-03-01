b014d1225c0d7a7f9548f9e900f3597740903beb
Bryan Leonard <bryan@forcequit.io>
2025-02-28 20:31:21 -0500

    Updating styling, begin player section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import Player from './components/Player';
 
 function App() {
   
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+		<main>
+			<div className="game-container">
+				<ol className="players">
+          <Player name={`Player 1`} symbol={`X`} />
+					<Player name={`Player 2`} symbol={`O`} />
+				</ol>
+				GAME
+			</div>
+			LOG
+		</main>
+  );
 }
 
 export default App
