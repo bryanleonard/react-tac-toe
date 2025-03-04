fc7466eaf9b8b7bb31a861dd0119eac2d7b3fd61
Bryan Leonard <bryan@forcequit.io>
2025-03-04 18:22:08 -0500

    Starter functionality for game play

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,3 +1,6 @@
+
+import { useState } from 'react';
+
 const initialGameBoard = [
 	[null, null, null],
 	[null, null, null],
@@ -5,14 +8,30 @@ const initialGameBoard = [
 ];
 
 export default function Gameboard() {
+
+	const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+	function handleSelectedSquare(rowIndex, colIndex) {
+		setGameBoard((prevGameBoard) => {
+			// anti-pattern - objects and arrays shold not mutate directly bc they are reference values. Do a deep copy and use that.
+			// prevGameBoard[rowIndex][colIndex] = 'X';
+			// It updates the orginal value in memory beofre the scheduled state change happens.
+			// NO const newUser = user
+			// YES const newUser = { ...user }
+			const updatedBoard = [...prevGameBoard.map(innerArry => [...innerArry])] //deep copy of nested arrays
+			updatedBoard[rowIndex][colIndex] = 'X'
+			return updatedBoard;
+		});
+	}
+
 	return (
 		<ol className="game-board">
-			{initialGameBoard.map((row, rowIndex) => (
+			{gameBoard.map((row, rowIndex) => (
 				<li key={rowIndex}>
 					<ol>
 						{row.map((playerSymbol, colIndex) => (
 							<li key={colIndex}>
-								<button>{playerSymbol}</button>
+								<button onClick={() => handleSelectedSquare(rowIndex, colIndex)}>{playerSymbol}</button>
 							</li>
 						))}
 					</ol>
