551434bfb957635cc2ae735ae57670d90e527f08
Bryan Leonard <bryan@forcequit.io>
2025-03-03 21:10:37 -0500

    Add basic gameboard, adjust responsive settings.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import Player from './components/Player';
-import Gameboard from './components/Gameboard';
+import Player from "./components/Player";
+import Gameboard from "./components/Gameboard";
 
 function App() {
-  
-
-  return (
+	return (
 		<main>
 			<div className="game-container">
 				<ol className="players">
-          <Player initialName={`Player 1`} symbol={`X`} />
+					<Player initialName={`Player 1`} symbol={`X`} />
 					<Player initialName={`Player 2`} symbol={`O`} />
 				</ol>
-				
-        <Gameboard />
-        
+
+				<Gameboard />
 			</div>
 			LOG
 		</main>
-  );
+	);
 }
 
-export default App
+export default App;
