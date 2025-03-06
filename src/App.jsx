b8e4b5f967818c1e9b45de83cea3b04752f32bdc
Bryan Leonard <bryan@forcequit.io>
2025-03-05 22:13:03 -0500

    Add logs

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
+import { useState } from 'react';
 import Player from "./components/Player";
 import Gameboard from "./components/Gameboard";
+import Log from './components/Log';
 
 function App() {
+	const [gameTurns, setGameTurns] = useState([]);
+	const [activePlayer, setActivePlayer] = useState('X');
+	
+	function handleSelectSquare(rowIdx, colIdx) {
+		setActivePlayer((currActivePlayer) => currActivePlayer === 'X' ? 'O' : 'X');
+		setGameTurns(prevTurns => {
+			let currPlayer = 'X';
+
+			if (prevTurns.length > 0 && prevTurns[0].player ==='X') {
+				currPlayer = 'O';
+			}
+			const updatedTurns = [
+				{
+					square: { row: rowIdx, col: colIdx }, 
+					player: currPlayer
+				}, 
+				...prevTurns];
+
+			return updatedTurns;
+		});
+	}
+
 	return (
 		<main>
 			<div className="game-container">
-				<ol className="players">
-					<Player initialName={`Player 1`} symbol={`X`} />
-					<Player initialName={`Player 2`} symbol={`O`} />
+				<ol className="players highlight-player">
+					<Player initialName={`Player 1`} symbol={`X`} isActive={activePlayer === 'X'} />
+					<Player initialName={`Player 2`} symbol={`O`} isActive={activePlayer === 'O'} />
 				</ol>
 
-				<Gameboard />
+				<Gameboard onSelectSquare={handleSelectSquare} turns={gameTurns} />
 			</div>
-			LOG
+			
+			<Log turns={gameTurns} />
 		</main>
 	);
 }
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,27 +1,38 @@
 
-import { useState } from 'react';
-
 const initialGameBoard = [
 	[null, null, null],
 	[null, null, null],
 	[null, null, null]
 ];
 
-export default function Gameboard() {
+// export default function Gameboard({ onSelectSquare, activePlayerSymbol }) {
+export default function Gameboard({ onSelectSquare, turns }) {
 
-	const [gameBoard, setGameBoard] = useState(initialGameBoard);
+	// const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
-	function handleSelectedSquare(rowIndex, colIndex) {
-		setGameBoard((prevGameBoard) => {
-			// anti-pattern - objects and arrays shold not mutate directly bc they are reference values. Do a deep copy and use that.
-			// prevGameBoard[rowIndex][colIndex] = 'X';
-			// It updates the orginal value in memory beofre the scheduled state change happens.
-			// NO const newUser = user
-			// YES const newUser = { ...user }
-			const updatedBoard = [...prevGameBoard.map(innerArry => [...innerArry])] //deep copy of nested arrays
-			updatedBoard[rowIndex][colIndex] = 'X'
-			return updatedBoard;
-		});
+	// function handleSelectedSquare(rowIndex, colIndex) {
+	// 	setGameBoard((prevGameBoard) => {
+	// 		// anti-pattern - objects and arrays shold not mutate directly bc they are reference values. Do a deep copy and use that.
+	// 		// prevGameBoard[rowIndex][colIndex] = 'X';
+	// 		// It updates the orginal value in memory beofre the scheduled state change happens.
+	// 		// NO const newUser = user
+	// 		// YES const newUser = { ...user }
+	// 		const updatedBoard = [...prevGameBoard.map(innerArry => [...innerArry])] //deep copy of nested arrays
+	// 		updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+	// 		return updatedBoard;
+	// 	});
+
+	// 	onSelectSquare();
+	// }
+
+	// Let's get some derive state action going...
+	let gameBoard = initialGameBoard;
+
+	for (const turn of turns) {
+		const { square, player } = turn;
+		const { row, col } = square;
+
+		gameBoard[row][col] = player;
 	}
 
 	return (
@@ -31,7 +42,8 @@ export default function Gameboard() {
 					<ol>
 						{row.map((playerSymbol, colIndex) => (
 							<li key={colIndex}>
-								<button onClick={() => handleSelectedSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+								{/* <button onClick={() => handleSelectedSquare(rowIndex, colIndex)}>{playerSymbol}</button> */}
+								<button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
 							</li>
 						))}
 					</ol>
