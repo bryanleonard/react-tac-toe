d9ec32d3c4ac2d4e33aa0b671bb23fd7319d6518
Bryan Leonard <bryan@forcequit.io>
2025-03-08 17:48:14 -0500

    Refactor app.js

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ import Log from './components/Log';
 import { WINNING_COMBINATIONS } from './winning-combos';
 import GameOver from './components/GameOver';
 
-const initialGameBoard = [
+const PLAYERS = {
+	X: 'Player 1',
+	O: 'Player 2'
+}
+
+const INITIL_GAME_BOARD = [
 	[null, null, null],
 	[null, null, null],
 	[null, null, null]
@@ -21,26 +26,7 @@ function deriveCurrPlayer(gameTurns) {
 	return currPlayer;
 }
 
-function App() {
-	const [players, serPlayers ]= useState({
-		'X': 'Player 1',
-		'O': 'Player 2'
-	});
-	const [gameTurns, setGameTurns] = useState([]);
-	// const [activePlayer, setActivePlayer] = useState('X');
-
-	const activePlayer = deriveCurrPlayer(gameTurns);
-
-	// Let's get some derive state action going...
-	let gameBoard = [...initialGameBoard.map(arry => [...arry])];
-
-	for (const turn of gameTurns) {
-		const { square, player } = turn;
-		const { row, col } = square;
-
-		gameBoard[row][col] = player;
-	}
-
+function deriveWinner(gameBoard, players) {
 	let winner = null;
 	let winnerSymbol = null;
 
@@ -57,6 +43,34 @@ function App() {
 		}
 	}
 
+	return {
+		winner,
+		winnerSymbol
+	};
+}
+
+function deriveGameBoard(gameTurns) {
+	// Let's get some derive state action going...
+	let gameBoard = [...INITIL_GAME_BOARD.map(arry => [...arry])];
+
+	for (const turn of gameTurns) {
+		const { square, player } = turn;
+		const { row, col } = square;
+
+		gameBoard[row][col] = player;
+	}
+
+	return gameBoard;
+}
+
+function App() {
+	const [players, setPlayers ]= useState(PLAYERS);
+	const [gameTurns, setGameTurns] = useState([]);
+
+	const activePlayer = deriveCurrPlayer(gameTurns);
+	const gameBoard = deriveGameBoard(gameTurns);
+
+	const {winner, winnerSymbol} = deriveWinner(gameBoard, players);
 	const hasDraw = gameTurns.length === 9 && !winner;
 	
 	function handleSelectSquare(rowIdx, colIdx) {
@@ -92,8 +106,8 @@ function App() {
 		<main>
 			<div className="game-container">
 				<ol className="players highlight-player">
-					<Player initialName={`Player 1`} symbol={`X`} isActive={activePlayer === 'X'} onChangeName={handlePlayerNameChange} />
-					<Player initialName={`Player 2`} symbol={`O`} isActive={activePlayer === 'O'} onChangeName={handlePlayerNameChange} />
+					<Player initialName={PLAYERS.X} symbol={`X`} isActive={activePlayer === 'X'} onChangeName={handlePlayerNameChange} />
+					<Player initialName={PLAYERS.O} symbol={`O`} isActive={activePlayer === 'O'} onChangeName={handlePlayerNameChange} />
 				</ol>
 
 				{(winner || hasDraw) && <GameOver winner={winner} winnerSymbol={winnerSymbol} onRestart={handleRestart} />}
